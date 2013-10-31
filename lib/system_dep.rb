8606c57be1aaa5086985f9e8d7fde272218caaef
Roland Laurès <roland.laures@gnuside.com>
2013-10-31 17:25:21 +0100

    added draft for the scripts/modules

---
+++ lib/system_dep.rb
@@ -0,0 +1,19 @@
+
+# This module is made to contains system dependant functions.
+# It is intend to regroup them in one module.
+#
+# Except when notified, every function will return 0 on sucess, and 1 on failure
+module SystemDep
+
+	# Allow to install a package given the program name.
+	def packageInstall
+		puts "To be implemented..."
+		return 1
+	end
+
+	# Allow to add a package source.
+	def packageAddSource
+		puts "To be implemented..."
+		return 1
+	end
+end
